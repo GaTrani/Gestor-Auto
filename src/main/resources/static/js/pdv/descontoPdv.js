6a037d581f842487372a90a8aa7ca50159a3425f
GaTrani <gabrieltrani02@gmail.com>
2024-05-30 00:23:58 -0300

    :sparkles: feat: Funcionalidade de desconto R$ e % - Sem produto

--- src/main/resources/static/js/pdv/descontoPdv.js
+++ src/main/resources/static/js/pdv/descontoPdv.js
@@ -3,11 +3,18 @@ $(document).ready(function () {
     var descontoAnteriorEmReais = 0; // Valor do desconto anterior em reais
 
     // Evento para botões de tipo de desconto
-    $('.btn-group button').click(function () {
-        $('.btn-group button').removeClass('btn-primary').addClass('btn-secondary');
-        $(this).removeClass('btn-secondary').addClass('btn-primary');
-
-        tipoDesconto = $(this).text();
+    $('.btn-desconto-group button').click(function () {
+        if ($(this).hasClass('btn-primary')) {
+            // Deselecionar o botão se ele já estiver selecionado
+            $(this).removeClass('btn-primary').addClass('btn-secondary');
+            tipoDesconto = null;
+            $('#desconto').val('');
+            descontoAnteriorEmReais = 0;
+        } else {
+            $('.btn-desconto-group button').removeClass('btn-primary').addClass('btn-secondary');
+            $(this).removeClass('btn-secondary').addClass('btn-primary');
+            tipoDesconto = $(this).text();
+        }
         ajustarCampoDesconto();
         aplicarDesconto();
     });
@@ -19,40 +26,52 @@ $(document).ready(function () {
 
     // Função para ajustar o campo de desconto conforme o tipo de desconto
     function ajustarCampoDesconto() {
+        var totalProdutos = parseFloat(calcularTotalProdutos()) || 0;
         if (tipoDesconto === 'R$') {
             $('#desconto').val(descontoAnteriorEmReais.toFixed(2));
             $('#desconto').attr('step', '0.01');
-            $('#desconto').attr('max', calcularTotalProdutos());
-        } else {
-            var totalProdutos = parseFloat(calcularTotalProdutos());
-            var descontoPercentual = (descontoAnteriorEmReais / totalProdutos) * 100;
-            $('#desconto').val(descontoPercentual.toFixed(2));
+            $('#desconto').attr('max', totalProdutos);
+        } else if (tipoDesconto === '%') {
+            if (totalProdutos > 0) {
+                var descontoPercentual = (descontoAnteriorEmReais / totalProdutos) * 100;
+                $('#desconto').val(descontoPercentual.toFixed(2));
+            } else {
+                $('#desconto').val('0.00');
+            }
             $('#desconto').attr('step', '0.01');
             $('#desconto').attr('max', '100');
+        } else {
+            $('#desconto').val('');
         }
     }
 
     // Função para aplicar o desconto
     function aplicarDesconto() {
-        var totalProdutos = parseFloat(calcularTotalProdutos());
+        var totalProdutos = parseFloat(calcularTotalProdutos()) || 0;
         var descontoValor = parseFloat($('#desconto').val().replace(',', '.')) || 0; // Replace comma with dot for decimal values
 
         var valorComDesconto;
 
-        if (tipoDesconto === '%') {
+        if (totalProdutos === 0) {
+            valorComDesconto = 0;
+            descontoAnteriorEmReais = 0;
+        } else if (tipoDesconto === '%') {
             if (descontoValor > 100) {
                 descontoValor = 100;
                 $('#desconto').val(100);
             }
             descontoAnteriorEmReais = (totalProdutos * (descontoValor / 100));
             valorComDesconto = totalProdutos - descontoAnteriorEmReais;
-        } else {
+        } else if (tipoDesconto === 'R$') {
             if (descontoValor > totalProdutos) {
                 descontoValor = totalProdutos;
                 $('#desconto').val(totalProdutos.toFixed(2));
             }
             descontoAnteriorEmReais = descontoValor;
             valorComDesconto = totalProdutos - descontoAnteriorEmReais;
+        } else {
+            descontoAnteriorEmReais = 0;
+            valorComDesconto = totalProdutos;
         }
 
         $('#valorTotalNota').val(totalProdutos.toFixed(2));
@@ -60,12 +79,17 @@ $(document).ready(function () {
     }
 
     // Evento para selecionar forma de pagamento
-    $('.btn-group.btn-group-toggle button').click(function () {
-        $('.btn-group.btn-group-toggle button').removeClass('btn-primary').addClass('btn-secondary');
-        $(this).removeClass('btn-secondary').addClass('btn-primary');
-
-        var formaPagamento = $(this).text();
-        $('#formaPagamento').val(formaPagamento);
+    $('.btn-pagamento-group button').click(function () {
+        if ($(this).hasClass('btn-primary')) {
+            // Deselecionar o botão se ele já estiver selecionado
+            $(this).removeClass('btn-primary').addClass('btn-secondary');
+            $('#formaPagamento').val('');
+        } else {
+            $('.btn-pagamento-group button').removeClass('btn-primary').addClass('btn-secondary');
+            $(this).removeClass('btn-secondary').addClass('btn-primary');
+            var formaPagamento = $(this).text();
+            $('#formaPagamento').val(formaPagamento);
+        }
     });
 
     // Função para calcular total dos produtos (mantida do seu código original)
